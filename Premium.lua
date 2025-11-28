a2421a0d73cb317b4a3e8e56f3bb6c3a05933da9
mascaracathub <mascaracathub@gmail.com>
2025-11-29 03:23:03 +0700

    Premium.lua

---
+++ Premium.lua
@@ -0,0 +1,15 @@
+if Premium then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/mascaracathub/Test-Script/refs/heads/main/Protected_2814004259330302.lua.txt"))()
+else
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/mascaracathub/Test-Script/refs/heads/main/Protected_2578153094727553.lua.txt"))()
+end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mascaracathub/Test-Script/refs/heads/main/Protected_5678912757027947.txt"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/mascaracathub/Test-Script/refs/heads/main/Protected_2677459089970026.txt"))()
+end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mascaracathub/Test-Script/refs/heads/main/AnhTuan.txt"))()
+else
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/mascaracathub/Test-Script/refs/heads/main/Arsenal.lua.txt"))()
+end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mascaracathub/Test-Script/refs/heads/main/Fish-Main.lua.txt"))()
+else
